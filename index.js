1e92a1ca7319d16bafa26106cf46ad030abc79e7
Andrew Shapovalov <ashapovalov@sharpminds.com>
2018-09-17 12:31:06 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const LABELS = require('./src/const/labels');
-const EN = require('./src/locales/en');
-const NL = require('./src/locales/nl');
+const LABELS = require('./src/const/labels')
+const EN = require('./src/locales/en')
+const NL = require('./src/locales/nl')
 
 module.exports = {
   LABELS,
